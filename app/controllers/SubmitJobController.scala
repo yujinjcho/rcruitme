286f6a247f25ce62b1378183298edf789b66486d
Andy Chiu <andy.chiu@refinery29.com>
2018-12-27 18:24:57 -0800

    submitjob routes

---
+++ app/controllers/SubmitJobController.scala
@@ -0,0 +1,27 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+
+import forms.JobForm
+
+class SubmitJobController @Inject()(
+  cc: ControllerComponents
+) extends AbstractController(cc) with I18nSupport {
+
+  def view() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.submitJob(JobForm.form))
+  }
+
+  def submit() = Action { implicit request: Request[AnyContent] =>
+    JobForm.form.bindFromRequest.fold(
+      form => BadRequest(views.html.submitJob(form)),
+      data => {
+        Redirect(routes.SubmitJobController.view()).flashing("info" -> "Job submitted.")
+      }
+    )
+  }
+
+}

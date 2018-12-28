8c926ff26dea4fc01c9cfdd14db54110b03832a6
Andy Chiu <andy.chiu@refinery29.com>
2018-12-28 03:07:34 -0800

    insert job on form submit

--- app/controllers/SubmitJobController.scala
+++ app/controllers/SubmitJobController.scala
@@ -4,22 +4,37 @@ import javax.inject._
 import play.api._
 import play.api.i18n.I18nSupport
 import play.api.mvc._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 import forms.JobForm
+import models.Job
+import models.services.JobService
 
 class SubmitJobController @Inject()(
-  cc: ControllerComponents
+  cc: ControllerComponents,
+  jobService: JobService
 ) extends AbstractController(cc) with I18nSupport {
 
   def view() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.submitJob(JobForm.form))
   }
 
-  def submit() = Action { implicit request: Request[AnyContent] =>
+  def submit() = Action.async { implicit request: Request[AnyContent] =>
     JobForm.form.bindFromRequest.fold(
-      form => BadRequest(views.html.submitJob(form)),
+      form => Future(BadRequest(views.html.submitJob(form))),
       data => {
-        Redirect(routes.SubmitJobController.view()).flashing("info" -> "Job submitted.")
+        val job = Job(
+          role = data.role,
+          company = data.company,
+          compensation = data.compensation,
+          location = data.location,
+          description = data.description,
+          benefits = data.benefits
+        )
+        jobService.insert(job).map { _ =>
+          Redirect(routes.SubmitJobController.view()).flashing("info" -> "Job submitted.")
+        }
       }
     )
   }

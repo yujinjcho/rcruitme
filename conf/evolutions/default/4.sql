481f315a36561a584525b67f9e0f7d02770a7372
Andy Chiu <andy.chiu@refinery29.com>
2018-12-28 03:06:35 -0800

    migration for jobs table

---
+++ conf/evolutions/default/4.sql
@@ -0,0 +1,19 @@
+# jobs schema
+
+# --- !Ups
+
+create table jobs (
+  id serial,
+  role varchar(255) not null,
+  company varchar(255) not null,
+  compensation int not null,
+  location varchar(255) not null,
+  description text not null,
+  benefits text,
+  viewed boolean not null default false,
+  submitted_at timestamp not null
+);
+
+# --- !Downs
+
+drop table jobs;

6b2018769ea9d7604cba87073f5708d9066fffe4
achiu8 <achiu8@gmail.com>
2019-01-15 14:08:16 -0500

    move writes to user companion object

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -16,17 +16,6 @@ class UserController @Inject() (
   silhouette: Silhouette[DefaultEnv]
 ) extends AbstractController(cc) {
 
-  implicit val userWrite = new Writes[User] {
-    def writes(user: User) = Json.obj(
-      "id" -> user.userID,
-      "firstName" -> user.firstName,
-      "lastName" -> user.lastName,
-      "email" -> user.email,
-      "type" -> user.userType.toString,
-      "activated" -> user.activated
-    )
-  }
-
   def get = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
     val user: User = request.identity
     Future.successful(Ok(Json.toJson(user)))

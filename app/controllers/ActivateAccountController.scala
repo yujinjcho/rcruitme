f6c8728d4347409d6e00c0dc24291339e049b093
Yujin <yujinjcho@gmail.com>
2019-02-20 21:01:03 -0800

    create connection if recruiter creates account via shared link

--- app/controllers/ActivateAccountController.scala
+++ app/controllers/ActivateAccountController.scala
@@ -1,22 +1,23 @@
 package controllers
 
-import scala.concurrent.{ ExecutionContext, Future }
-import scala.util.{Success,Failure}
-
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 import com.mohiva.play.silhouette.api._
 import javax.inject.Inject
 import com.mohiva.play.silhouette.api.crypto.CrypterAuthenticatorEncoder
 import com.mohiva.play.silhouette.impl.authenticators.{JWTAuthenticator, JWTAuthenticatorSettings}
 import play.api.i18n.I18nSupport
-import play.api.mvc.{ AbstractController, ControllerComponents }
-
+import play.api.mvc.{AbstractController, ControllerComponents}
+import models.daos.ConnectionDAO
 import models.services.UserService
+import models.{User, UserType}
 import utils.auth.DefaultEnv
 
 class ActivateAccountController @Inject() (
   cc: ControllerComponents,
   silhouette: Silhouette[DefaultEnv],
   userService: UserService,
+  connectionDAO: ConnectionDAO,
   settings: JWTAuthenticatorSettings,
   authenticatorEncoder: CrypterAuthenticatorEncoder,
   )(implicit ex: ExecutionContext
@@ -27,6 +28,8 @@ class ActivateAccountController @Inject() (
       case Success(authenticator) =>
         userService.retrieve(authenticator.loginInfo).flatMap {
           case Some(user) =>
+            createConnectionIfSharedLink(redirect, user)
+
             userService.update(user.copy(activated = true)).map { _ =>
               Redirect(redirect)
             }
@@ -35,6 +38,23 @@ class ActivateAccountController @Inject() (
         }
       case Failure(_) =>
         Future.successful(Ok("issues with activation link"))
+    }
+  }
+
+  private def createConnectionIfSharedLink(redirect: String, user: User): Unit = {
+    if (user.userType == UserType.Recruiter) {
+      extractCandidateId(redirect).map { candidateId =>
+        userService.retrieve(candidateId).map { candidate =>
+          connectionDAO.create(candidate.userID, user.userID)
+        }
+      }
+    }
+  }
+
+  private def extractCandidateId(redirect: String): Option[Int] = {
+    redirect.split("/").takeRight(2) match {
+      case Array("submit-job", candidateId)  => Some(candidateId.toInt)
+      case _ => None
     }
   }
 }
